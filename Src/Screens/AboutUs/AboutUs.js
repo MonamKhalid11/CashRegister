8eae319316059c13045c88d4900b46f622a5c9bc
Hashaam Ahmad <hashaam@11-seas.com>
2022-02-02 16:01:35 +0500

    feedback changes

--- Src/Screens/AboutUs/AboutUs.js
+++ Src/Screens/AboutUs/AboutUs.js
@@ -1,79 +1,82 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {
-    SafeAreaView,
-    ScrollView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    useColorScheme,
-    View, TextInput, Button, Touchable, TouchableOpacity, useWindowDimensions
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  TextInput,
+  Button,
+  Touchable,
+  TouchableOpacity,
+  useWindowDimensions,
+  Image,
 } from 'react-native';
-import HeaderComponent from '../../Components/HeaderComponent/HeaderComponent'
-import { widthPercentageToDP as wp, heightPercentageToDP as hp, heightPercentageToDP } from 'react-native-responsive-screen';
-import styles from './Styles'
+import HeaderComponent from '../../Components/HeaderComponent/HeaderComponent';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+  heightPercentageToDP,
+} from 'react-native-responsive-screen';
+import styles from './Styles';
 import RenderHtml from 'react-native-render-html';
-import { useDispatch, useSelector } from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux';
+import Logo from '../../Assets/logo.png';
+const AboutUs = ({navigation}) => {
+  const {toggleDrawer} = navigation; // <-- drawer's navigation (not from stack)
+  const tokenChecked = useSelector(state => state?.listing?.tokenChecked);
+  const {width} = useWindowDimensions();
+  const [first, setFirst] = useState(false);
 
+  useEffect(() => {
+    if (tokenChecked == 'peppermint') {
+      setFirst(true);
+    } else if (tokenChecked == 'givingtree') {
+      setFirst(false);
+    }
+  }, []);
 
-const AboutUs = ({ navigation }) => {
-    const { toggleDrawer } = navigation // <-- drawer's navigation (not from stack)
-    const tokenChecked = useSelector(state => state?.listing?.tokenChecked)
-    const { width } = useWindowDimensions();
-    const [first, setFirst] = useState(false)
-
-    useEffect(() => {
-        if (tokenChecked == "peppermint") {
-            setFirst(true)
-        } else if (tokenChecked == "givingtree") {
-            setFirst(false)
-        }
-    }, [])
-
-    const source = {
-        html: `
+  const source = {
+    html: `
       <p style='text-align:center;font-size:20px;font-weight:'bold';>
       Peppermint Lane is an in-school shopping program that offers gift items at reasonable prices for everyone on a child's shopping list. Our goal is to provide quality merchandise and superior customer service for your Holiday Shop. Please contact us for more information at 631-804-2762 or
         <a href="url">mkarusch@gmail.com</a>
-      </p>`
-    };
-    const source2 = {
-        html: `
+      </p>`,
+  };
+  const source2 = {
+    html: `
       <p style='text-align:center;font-size:20px;font-weight:'bold';>
       The Giving Tree is an in-school shopping program that offers gift items at reasonable prices for everyone on a child's shopping list. Our goal is to provide quality merchandise and superior customer service for your Holiday Shop. Please contact us for more information at 502-592-5325 or
         <a href="url">JoysGifts4Kids@aol.com</a>
-      </p>`
-    };
+      </p>`,
+  };
 
-
-    return (
-        <View style={{ flex: 1 }}>
-            <View style={styles.headerView}>
-                <HeaderComponent
-                    CashRegister={"About Us"}
-                    openDrawer={toggleDrawer}
-
-                />
-            </View>
-
-            <View style={styles.htmlView}>
-
-                <View style={styles.innerView}>
-                    <RenderHtml
-                        contentWidth={width}
-                        source={first ? source : source2}
-                    />
-                </View>
-
-
-            </View>
-
-
-
-
-
-
+  return (
+    <ScrollView bounces={0}>
+      <View style={{flex: 1}}>
+        <View style={styles.headerView}>
+          <HeaderComponent
+            CashRegister={'About Us'}
+            openDrawer={toggleDrawer}
+          />
         </View>
-    )
-
-}
-export default AboutUs
+        <View style={styles.htmlView}>
+          <View style={styles.innerView}>
+            <RenderHtml
+              contentWidth={width}
+              source={first ? source : source2}
+            />
+            {first ? (
+              <Image source={Logo} style={styles.image} />
+            ) : (
+              <Image source={Logo} style={styles.image} />
+            )}
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+export default AboutUs;
--- Src/Screens/AboutUs/Styles.js
+++ Src/Screens/AboutUs/Styles.js
@@ -1,16 +1,29 @@
-import { StyleSheet } from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler'
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import {StyleSheet} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
 export default StyleSheet.create({
-
-    headerView: {
-        flex: 0.08, marginTop: wp(7),
-    },
-    htmlView: {
-        flex: 0.98, marginTop: wp(2),
-    },
-    innerView: {
-        height: hp(40), width: wp(95), marginTop: wp(10), marginLeft: wp(2.5)
-    }
-})
+  headerView: {
+    flex: 0.08,
+    marginTop: wp(7),
+  },
+  htmlView: {
+    flex: 0.98,
+    marginTop: wp(2),
+  },
+  innerView: {
+    width: wp(95),
+    marginTop: wp(10),
+    marginLeft: wp(2.5),
+    flex: 1,
+  },
+  image: {
+    width: wp('100'),
+    height: wp('65'),
+    resizeMode: 'contain',
+    alignSelf: 'center',
+  },
+});
